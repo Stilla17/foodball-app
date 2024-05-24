a63cdd35adeea2185da6863a8ee6a601bba494b5
Stilla <sunnatilla95@gmail.com>
2024-05-24 17:07:56 +0500

    mui qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import Alert from '@mui/material/Alert';
+import CheckIcon from '@mui/icons-material/Check';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function App() {
 
   return (
     <>
-      <h1></h1>
+      <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+        Serkan
+      </Alert>
+
+      <Box
+        component="form"
+        sx={{
+          backgroundColor: 'red', color: 'red',
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField sx={{ color: "red" }} id="outlined-basic" label="Serkan" variant="outlined" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+      </Box>
+
     </>
   )
 }
