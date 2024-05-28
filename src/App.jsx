669df7b5cf720ba38f34442a0c87be8db6631089
Stilla <sunnatilla95@gmail.com>
2024-05-28 08:26:54 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-
+import Commmon from './Component/Commmon'
+import { Routes, Route } from 'react-router-dom';
+import Last from './Component/Last';
 function App() {
 
   return (
     <>
-  
+      <Routes>
+        <Route path="/" element={<Commmon />} />
+        <Route path="/main" element={<Last />} />
+      </Routes>
     </>
   )
 }
---
+++ src/Component/Commmon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Commmon() {
+    return (
+        <div>
+            <Link to="/main">hello</Link>
+        </div>
+    )
+}
+
+export default Commmon
