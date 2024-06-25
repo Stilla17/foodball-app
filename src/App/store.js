47e8f5f8c0e1d0c7e69a50b97d2f5b8f26caf607
hasansuh <khasansux@gamil.com>
2024-06-25 18:48:39 +0500

    fix folder

--- src/App/store.js
+++ src/App/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../Component/counterSlice/counterSlice";
+import counterReducer from "../features/counterSlice/counterSlice";
+import switcherReducer from "../features/switch/switchSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    switcher: switcherReducer,
   },
 });
 
